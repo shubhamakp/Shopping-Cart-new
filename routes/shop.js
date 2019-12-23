044f355d2517317ce1c4248d3152ed71a3f1dafb
shubhamakp <shubhamakp@gmail.com>
2019-12-24 00:21:59 +0530

    First commit

---
+++ routes/shop.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+ // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+  res.render('shop', {prods: products, docTitle: 'Shop'});
+});
+
+module.exports = router;
